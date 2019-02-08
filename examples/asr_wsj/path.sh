079a24dad98117555e690f6290cbe0b89948379b
freewym <freewym@gmail.com>
2019-02-08 00:05:55 -0500

    environment configurations

--- examples/asr_wsj/path.sh
+++ examples/asr_wsj/path.sh
@@ -1,7 +1,14 @@
-export PATH=~/anaconda3/bin:$PATH
-
 MAIN_ROOT=$PWD/../..
+KALDI_ROOT=$MAIN_ROOT/speech_tools/kaldi
 
-export PATH=$MAIN_ROOT:$PATH
+# BEGIN from kaldi path.sh
+[ -f $KALDI_ROOT/tools/env.sh ] && . $KALDI_ROOT/tools/env.sh
+export PATH=$PWD/utils/:$KALDI_ROOT/tools/openfst/bin:$KALDI_ROOT/tools/sctk/bin:$PWD:$PATH
+[ ! -f $KALDI_ROOT/tools/config/common_path.sh ] && echo >&2 "The standard file $KALDI_ROOT/tools/config/common_path.sh is not present -> Exit!" && exit 1
+. $KALDI_ROOT/tools/config/common_path.sh
 export LC_ALL=C
+# END
+
+export PATH=~/anaconda3/bin:$PATH
+export PATH=$MAIN_ROOT:$PATH
 
