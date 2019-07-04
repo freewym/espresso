0e7fb863f624bc9ef75e44980d53e2ef7c38f545
freewym <freewym@gmail.com>
2019-07-04 12:29:43 -0400

    fix swbd recipe; code adaptation/changes according to the commits from Jul 17, 2019 to Jul 19, 2019

---
+++ examples/asr_swbd/local/score.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+# Copyright (c) 2012,         Johns Hopkins University (Author: Daniel Povey)
+#               2019-present, Yiming Wang
+# All rights reserved.
+
+
+orig_args=
+for x in "$@"; do orig_args="$orig_args '$x'"; done
+
+# begin configuration section.  we include all the options that score_sclite.sh or
+# score_basic.sh might need, or parse_options.sh will die.
+# CAUTION: these default values do not have any effect because of the
+# way pass things through to the scripts that this script calls.
+cmd=run.pl
+#end configuration section.
+
+[ -f ./path.sh ] && . ./path.sh
+. parse_options.sh || exit 1;
+
+if [ $# -ne 2 ]; then
+  echo "Usage: local/score.sh [options] <data-dir> <decode-dir>" && exit;
+  echo " Options:"
+  echo "    --cmd (run.pl|queue.pl...)      # specify how to run the sub-processes."
+  exit 1;
+fi
+
+data=$1
+
+if [ -f $data/stm ]; then # use sclite scoring.
+  echo "$data/stm exists: using local/score_sclite.sh"
+  eval local/score_sclite.sh $orig_args
+else
+  echo "$data/stm does not exist: using local/score_basic.sh"
+  eval local/score_basic.sh $orig_args
+fi
