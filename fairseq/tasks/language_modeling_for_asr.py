4233dff7ae9f819b762f8a43ce15b01b5101694d
freewym <freewym@gmail.com>
2019-09-20 14:45:59 -0400

    compansate for the removal of torch.rand() from distributed_init() recently introduced in fairseq, to make ASR results reproducible

--- fairseq/tasks/language_modeling_for_asr.py
+++ fairseq/tasks/language_modeling_for_asr.py
@@ -56,6 +56,10 @@ class LanguageModelingForASRTask(LanguageModelingTask):
     def __init__(self, args, dictionary, output_dictionary=None, targets=None):
         super().__init__(args, dictionary, output_dictionary, targets=targets)
         torch.backends.cudnn.deterministic = True
+        # Compansate for the removal of :func:`torch.rand()` from
+        # :func:`fairseq.distributed_utils.distributed_init()` by fairseq,
+        # to make previous experiments reproducible.
+        torch.rand(1)
 
     @classmethod
     def load_dictionary(cls, filename):
