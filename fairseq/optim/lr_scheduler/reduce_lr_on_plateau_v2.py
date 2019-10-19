011cb5e84e7e086311d7eaea3e40bb096988bcbf
freewym <freewym@gmail.com>
2019-10-18 22:04:51 -0400

    change warmup scheduling for ReduceLROnPlateauV2; code adaptation/changes according to the commits on Oct 18, 2019

--- fairseq/optim/lr_scheduler/reduce_lr_on_plateau_v2.py
+++ fairseq/optim/lr_scheduler/reduce_lr_on_plateau_v2.py
@@ -21,14 +21,13 @@ class ReduceLROnPlateauV2(ReduceLROnPlateau):
     def __init__(self, args, optimizer):
         super().__init__(args, optimizer)
 
-        if args.warmup_updates > 0:
-            self.warmup_factor = 1. / args.warmup_updates
-        else:
-            self.warmup_factor = 1.
+        self.init_lr = args.init_lr_scale * args.lr[0] if args.warmup_updates > 0 else args.lr[0]
+        self.warmup_rate = (args.lr[0] - self.init_lr) / args.warmup_updates \
+            if args.warmup_updates > 0 else 0.
 
         self.lr_scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
             self.optimizer.optimizer, patience=0, factor=args.lr_shrink,
-            threshold=args.lr_threshold, min_lr=args.min_lr)
+            threshold=args.lr_threshold, min_lr=args.final_lr_scale * args.lr[0])
     
     @staticmethod
     def add_args(parser):
@@ -37,6 +36,10 @@ class ReduceLROnPlateauV2(ReduceLROnPlateau):
         # fmt: off
         parser.add_argument('--warmup-updates', default=0, type=int, metavar='N',
                             help='warmup the learning rate linearly for the first N updates')
+        parser.add_argument('--init-lr-scale', default=0.01, type=float, metavar='N',
+                            help='initial learning rate scale during warmup phase; default is 0.01')
+        parser.add_argument('--final-lr-scale', default=0.01, type=float, metavar='N',
+                            help='final learning rate scale; default to 0.01')
         parser.add_argument('--start-reduce-lr-epoch', default=0, type=int, metavar='N',
                             help='start to reduce lr from the specified epoch')
         # fmt: on
@@ -44,13 +47,12 @@ class ReduceLROnPlateauV2(ReduceLROnPlateau):
     def step(self, epoch, val_loss=None):
         if epoch < self.args.start_reduce_lr_epoch:
             self.lr_scheduler.last_epoch = epoch
-            self.optimizer.set_lr(self.warmup_factor * self.args.lr[0])
+            self.optimizer.set_lr(self.args.lr[0])
             return self.optimizer.get_lr()
         return super().step(epoch, val_loss)
 
     def step_update(self, num_updates):
         """Update the learning rate after each update."""
         if self.args.warmup_updates > 0 and num_updates <= self.args.warmup_updates:
-            self.warmup_factor = num_updates / float(self.args.warmup_updates)
-            self.optimizer.set_lr(self.warmup_factor * self.args.lr[0])
+            self.optimizer.set_lr(self.init_lr + self.warmup_rate * num_updates)
         return self.optimizer.get_lr()
