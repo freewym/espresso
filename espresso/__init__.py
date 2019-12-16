978f6a631d42046dbdc4a9ee447dfab093649627
freewym <freewym@gmail.com>
2019-12-16 17:43:37 -0500

    re-organize the codebase to isolate espresso from fairseq

---
+++ espresso/__init__.py
@@ -0,0 +1,12 @@
+# Copyright (c) Yiming Wang
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+
+import espresso.criterions  # noqa
+import espresso.models  # noqa
+import espresso.modules  # noqa
+import espresso.optim  # noqa
+import espresso.optim.lr_scheduler  # noqa
+import espresso.tasks  # noqa
