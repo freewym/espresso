99281b03cc5c62f13ffd731e3ce8c78d8001d8b4
freewym <freewym@gmail.com>
2020-01-03 20:00:53 -0500

    code adaptation/changes according to the commits from Jan 3 to Jan 6, 2020

--- espresso/tasks/speech_recognition.py
+++ espresso/tasks/speech_recognition.py
@@ -7,7 +7,7 @@ import os
 
 import torch
 
-from fairseq import options
+from fairseq import options, search
 from fairseq.data import ConcatDataset
 
 from fairseq.tasks import FairseqTask, register_task
@@ -228,6 +228,48 @@ class SpeechRecognitionEspressoTask(FairseqTask):
             print('| --score-reference is not applicable to speech recognition,'
                   ' ignoring it.')
         from fairseq.sequence_generator import SequenceGenerator
+
+        # Choose search strategy. Defaults to Beam Search.
+        sampling = getattr(args, 'sampling', False)
+        sampling_topk = getattr(args, 'sampling_topk', -1)
+        sampling_topp = getattr(args, 'sampling_topp', -1.0)
+        diverse_beam_groups = getattr(args, 'diverse_beam_groups', -1)
+        diverse_beam_strength = getattr(args, 'diverse_beam_strength', 0.5),
+        match_source_len = getattr(args, 'match_source_len', False)
+        diversity_rate = getattr(args, 'diversity_rate', -1)
+        if (
+            sum(
+                int(cond)
+                for cond in [
+                    sampling,
+                    diverse_beam_groups > 0,
+                    match_source_len,
+                    diversity_rate > 0,
+                ]
+            )
+            > 1
+        ):
+            raise ValueError('Provided Search parameters are mutually exclusive.')
+        assert sampling_topk < 0 or sampling, '--sampling-topk requires --sampling'
+        assert sampling_topp < 0 or sampling, '--sampling-topp requires --sampling'
+
+        if sampling:
+            search_strategy = search.Sampling(self.target_dictionary, sampling_topk, sampling_topp)
+        elif diverse_beam_groups > 0:
+            search_strategy = search.DiverseBeamSearch(
+                self.target_dictionary, diverse_beam_groups, diverse_beam_strength)
+        elif match_source_len:
+            # this is useful for tagging applications where the output
+            # length should match the input length, so we hardcode the
+            # length constraints for simplicity
+            search_strategy = search.LengthConstrainedBeamSearch(
+                self.target_dictionary, min_len_a=1, min_len_b=0, max_len_a=1, max_len_b=0,
+            )
+        elif diversity_rate > -1:
+            search_strategy = search.DiverseSiblingsSearch(self.target_dictionary, diversity_rate)
+        else:
+            search_strategy = search.BeamSearch(self.target_dictionary)
+
         return SequenceGenerator(
             self.target_dictionary,
             beam_size=getattr(args, 'beam', 5),
@@ -237,14 +279,10 @@ class SpeechRecognitionEspressoTask(FairseqTask):
             normalize_scores=(not getattr(args, 'unnormalized', False)),
             len_penalty=getattr(args, 'lenpen', 1),
             unk_penalty=getattr(args, 'unkpen', 0),
-            sampling=getattr(args, 'sampling', False),
-            sampling_topk=getattr(args, 'sampling_topk', -1),
-            sampling_topp=getattr(args, 'sampling_topp', -1.0),
             temperature=getattr(args, 'temperature', 1.),
-            diverse_beam_groups=getattr(args, 'diverse_beam_groups', -1),
-            diverse_beam_strength=getattr(args, 'diverse_beam_strength', 0.5),
             match_source_len=getattr(args, 'match_source_len', False),
             no_repeat_ngram_size=getattr(args, 'no_repeat_ngram_size', 0),
+            search_strategy=search_strategy,
             eos_factor=getattr(args, 'eos_factor', None),
         )
 
