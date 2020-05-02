20e10d563215d6ec344bf8df11b6ecd200fb60da
Yiming Wang <freewym@gmail.com>
2020-05-02 19:53:55 -0400

    Hybrid ASR code (E2E LF-MMI and cross-entropy) and WSJ examples (#29)

--- espresso/data/__init__.py
+++ espresso/data/__init__.py
@@ -3,8 +3,10 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
+from .asr_chain_dataset import AsrChainDataset, NumeratorGraphDataset
 from .asr_dataset import AsrDataset
 from .asr_dictionary import AsrDictionary
+from .asr_xent_dataset import AliScpCachedDataset, AsrXentDataset
 from .feat_text_dataset import (
     AsrTextDataset,
     FeatScpCachedDataset,
@@ -13,10 +15,14 @@ from .feat_text_dataset import (
 )
 
 __all__ = [
+    'AliScpCachedDataset',
+    'AsrChainDataset',
     'AsrDataset',
     'AsrDictionary',
     'AsrTextDataset',
+    'AsrXentDataset',
     'FeatScpCachedDataset',
     'FeatScpDataset',
     'FeatScpInMemoryDataset',
+    'NumeratorGraphDataset',
 ]
