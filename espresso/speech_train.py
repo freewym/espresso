444719d981e8bc71f0166210b5ce2f026ba8ee93
freewym <freewym@gmail.com>
2020-06-19 01:34:20 -0400

    code adaptation/changes according to the commits on Jun 18-23, 2020

--- espresso/speech_train.py
+++ espresso/speech_train.py
@@ -16,7 +16,6 @@ import sys
 
 import numpy as np
 import torch
-
 from fairseq import (
     checkpoint_utils,
     distributed_utils,
@@ -27,28 +26,29 @@ from fairseq import (
 )
 from fairseq.data import iterators
 from fairseq.logging import meters, metrics, progress_bar
-from fairseq.trainer import Trainer
 from fairseq.model_parallel.megatron_trainer import MegatronTrainer
+from fairseq.trainer import Trainer
 
 
 logging.basicConfig(
-    format='%(asctime)s | %(levelname)s | %(name)s | %(message)s',
-    datefmt='%Y-%m-%d %H:%M:%S',
+    format="%(asctime)s | %(levelname)s | %(name)s | %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
     level=logging.INFO,
     stream=sys.stdout,
 )
-logger = logging.getLogger('espresso.speech_train')
+logger = logging.getLogger("espresso.speech_rain")
 
 
 def main(args, init_distributed=False):
     utils.import_user_module(args)
 
-    assert args.max_tokens is not None or args.max_sentences is not None, \
-        'Must specify batch size either with --max-tokens or --max-sentences'
+    assert (
+        args.max_tokens is not None or args.max_sentences is not None
+    ), "Must specify batch size either with --max-tokens or --max-sentences"
     metrics.reset()
 
     # Initialize CUDA and distributed training
-    if torch.cuda.is_available() and not args.cpu and not getattr(args, 'tpu', False):
+    if torch.cuda.is_available() and not args.cpu and not getattr(args, "tpu", False):
         torch.cuda.set_device(args.device_id)
     np.random.seed(args.seed)
     utils.set_torch_seed(args.seed)
@@ -65,18 +65,22 @@ def main(args, init_distributed=False):
     task = tasks.setup_task(args)
 
     # Load valid dataset (we load training data below, based on the latest checkpoint)
-    for valid_sub_split in args.valid_subset.split(','):
+    for valid_sub_split in args.valid_subset.split(","):
         task.load_dataset(valid_sub_split, combine=False, epoch=1)
 
     # Build model and criterion
     model = task.build_model(args)
     criterion = task.build_criterion(args)
     logger.info(model)
-    logger.info('model {}, criterion {}'.format(args.arch, criterion.__class__.__name__))
-    logger.info('num. model params: {} (num. trained: {})'.format(
-        sum(p.numel() for p in model.parameters()),
-        sum(p.numel() for p in model.parameters() if p.requires_grad),
-    ))
+    logger.info(
+        "model {}, criterion {}".format(args.arch, criterion.__class__.__name__)
+    )
+    logger.info(
+        "num. model params: {} (num. trained: {})".format(
+            sum(p.numel() for p in model.parameters()),
+            sum(p.numel() for p in model.parameters() if p.requires_grad),
+        )
+    )
 
     # (optionally) Configure quantization
     if args.quantization_config_path is not None:
@@ -94,18 +98,22 @@ def main(args, init_distributed=False):
     else:
         trainer = MegatronTrainer(args, task, model, criterion)
 
-    logger.info('training on {} devices (GPUs/TPUs)'.format(args.distributed_world_size))
-    logger.info('max input frames per GPU = {} and max sentences per GPU = {}'.format(
-        args.max_tokens,
-        args.max_sentences,
-    ))
+    logger.info(
+        "training on {} devices (GPUs/TPUs)".format(args.distributed_world_size)
+    )
+    logger.info(
+        "max input frames per GPU = {} and max sentences per GPU = {}".format(
+            args.max_tokens, args.max_sentences
+        )
+    )
 
     # Load the latest checkpoint if one is available and restore the
     # corresponding train iterator
     extra_state, epoch_itr = checkpoint_utils.load_checkpoint(args, trainer)
     if args.tpu:
         import torch_xla.core.xla_model as xm
-        xm.rendezvous('load_checkpoint')  # wait for all workers
+
+        xm.rendezvous("load_checkpoint")  # wait for all workers
         xm.mark_step()
 
     # Train until the learning rate gets too small
@@ -113,10 +121,7 @@ def main(args, init_distributed=False):
     lr = trainer.get_lr()
     train_meter = meters.StopwatchMeter()
     train_meter.start()
-    while (
-        lr > args.min_lr
-        and epoch_itr.next_epoch_idx <= max_epoch
-    ):
+    while lr > args.min_lr and epoch_itr.next_epoch_idx <= max_epoch:
         # train for one epoch
         valid_losses, should_stop = train(args, trainer, task, epoch_itr)
         if should_stop:
@@ -128,10 +133,10 @@ def main(args, init_distributed=False):
         epoch_itr = trainer.get_train_iterator(
             epoch_itr.next_epoch_idx,
             # sharded data: get train iterator for next epoch
-            load_dataset=(os.pathsep in getattr(args, 'data', '')),
+            load_dataset=(os.pathsep in getattr(args, "data", "")),
         )
     train_meter.stop()
-    logger.info('done training in {:.1f} seconds'.format(train_meter.sum))
+    logger.info("done training in {:.1f} seconds".format(train_meter.sum))
 
 
 def should_stop_early(args, valid_loss):
@@ -144,7 +149,7 @@ def should_stop_early(args, valid_loss):
     def is_better(a, b):
         return a > b if args.maximize_best_checkpoint_metric else a < b
 
-    prev_best = getattr(should_stop_early, 'best', None)
+    prev_best = getattr(should_stop_early, "best", None)
     if prev_best is None or is_better(valid_loss, prev_best):
         should_stop_early.best = valid_loss
         should_stop_early.num_runs = 0
@@ -152,7 +157,11 @@ def should_stop_early(args, valid_loss):
     else:
         should_stop_early.num_runs += 1
         if should_stop_early.num_runs >= args.patience:
-            logger.info('early stop since valid performance hasn\'t improved for last {} runs'.format(args.patience))
+            logger.info(
+                "early stop since valid performance hasn't improved for last {} runs".format(
+                    args.patience
+                )
+            )
             return True
         else:
             return False
@@ -161,17 +170,18 @@ def should_stop_early(args, valid_loss):
 def tpu_data_loader(args, itr):
     import torch_xla.core.xla_model as xm
     import torch_xla.distributed.parallel_loader as pl
-    xm.rendezvous('tpu_data_loader')  # wait for all workers
+
+    xm.rendezvous("tpu_data_loader")  # wait for all workers
     xm.mark_step()
     device = utils.get_tpu_device(args)
     return iterators.CountingIterator(
         pl.ParallelLoader(itr, [device]).per_device_loader(device),
-        start=getattr(itr, 'n', 0),
+        start=getattr(itr, "n", 0),
         total=len(itr),
     )
 
 
-@metrics.aggregate('train')
+@metrics.aggregate("train")
 def train(args, trainer, task, epoch_itr):
     """Train the model for one epoch and return validation losses."""
     # Initialize data iterator
@@ -185,7 +195,7 @@ def train(args, trainer, task, epoch_itr):
         else args.update_freq[-1]
     )
     itr = iterators.GroupedIterator(itr, update_freq)
-    if getattr(args, 'tpu', False):
+    if getattr(args, "tpu", False):
         itr = tpu_data_loader(args, itr)
     progress = progress_bar.progress_bar(
         itr,
@@ -195,18 +205,18 @@ def train(args, trainer, task, epoch_itr):
         tensorboard_logdir=(
             args.tensorboard_logdir if distributed_utils.is_master(args) else None
         ),
-        default_log_format=('tqdm' if not args.no_progress_bar else 'simple'),
+        default_log_format=("tqdm" if not args.no_progress_bar else "simple"),
     )
 
     trainer.begin_epoch(epoch_itr.epoch)
 
-    if hasattr(trainer.criterion, 'set_epoch'):
+    if hasattr(trainer.criterion, "set_epoch"):
         trainer.criterion.set_epoch(epoch_itr.epoch)
 
-    valid_subsets = args.valid_subset.split(',')
+    valid_subsets = args.valid_subset.split(",")
     should_stop = False
-    for samples in progress:
-        with metrics.aggregate('train_inner'):
+    for i, samples in enumerate(progress):
+        with metrics.aggregate("train_inner"), torch.autograd.profiler.record_function("train_step-%d" % i):
             log_output = trainer.train_step(samples)
             if log_output is None:  # OOM, overflow, ...
                 continue
@@ -214,17 +224,17 @@ def train(args, trainer, task, epoch_itr):
         # log mid-epoch stats
         num_updates = trainer.get_num_updates()
         if num_updates % args.log_interval == 0:
-            stats = get_training_stats(metrics.get_smoothed_values('train_inner'))
-            progress.log(stats, tag='train_inner', step=num_updates)
+            stats = get_training_stats(metrics.get_smoothed_values("train_inner"))
+            progress.log(stats, tag="train_inner", step=num_updates)
 
             # reset mid-epoch stats after each log interval
             # the end-of-epoch stats will still be preserved
-            metrics.reset_meters('train_inner')
+            metrics.reset_meters("train_inner")
 
         # update the state prior stored in the model for cross-entropy training
-        if hasattr(task, 'update_state_prior'):
+        if hasattr(task, "update_state_prior"):
             task.update_state_prior(trainer.get_model())
-
+        
         end_of_epoch = not itr.has_next()
         valid_losses, should_stop = validate_and_save(
             args, trainer, task, epoch_itr, valid_subsets, end_of_epoch
@@ -233,31 +243,25 @@ def train(args, trainer, task, epoch_itr):
             break
 
     # log end-of-epoch stats
-    stats = get_training_stats(metrics.get_smoothed_values('train'))
-    progress.print(stats, tag='train', step=num_updates)
+    stats = get_training_stats(metrics.get_smoothed_values("train"))
+    progress.print(stats, tag="train", step=num_updates)
 
     # reset epoch-level meters
-    metrics.reset_meters('train')
+    metrics.reset_meters("train")
     return valid_losses, should_stop
 
 
 def validate_and_save(args, trainer, task, epoch_itr, valid_subsets, end_of_epoch):
     num_updates = trainer.get_num_updates()
     do_save = (
-        (
-            args.save_interval_updates > 0
-            and num_updates > 0
-            and num_updates % args.save_interval_updates == 0
-        )
-        or (end_of_epoch and epoch_itr.epoch % args.save_interval == 0)
-    )
+        args.save_interval_updates > 0
+        and num_updates > 0
+        and num_updates % args.save_interval_updates == 0
+    ) or (end_of_epoch and epoch_itr.epoch % args.save_interval == 0)
     do_validate = (
-        (
-            (not end_of_epoch and do_save)  # validate during mid-epoch saves
-            or (end_of_epoch and epoch_itr.epoch % args.validate_interval == 0)
-        )
-        and not args.disable_validation
-    )
+        (not end_of_epoch and do_save)  # validate during mid-epoch saves
+        or (end_of_epoch and epoch_itr.epoch % args.validate_interval == 0)
+    ) and not args.disable_validation
 
     # Validate
     valid_losses = [None]
@@ -279,7 +283,7 @@ def validate_and_save(args, trainer, task, epoch_itr, valid_subsets, end_of_epoc
 
 
 def get_training_stats(stats):
-    stats['wall'] = round(metrics.get_meter('default', 'wall').elapsed_time, 0)
+    stats["wall"] = round(metrics.get_meter("default", "wall").elapsed_time, 0)
     return stats
 
 
@@ -294,7 +298,7 @@ def validate(args, trainer, task, epoch_itr, subsets):
     for subset in subsets:
         # Initialize data iterator
         itr = trainer.get_valid_iterator(subset).next_epoch_itr(shuffle=False)
-        if getattr(args, 'tpu', False):
+        if getattr(args, "tpu", False):
             itr = tpu_data_loader(args, itr)
         progress = progress_bar.progress_bar(
             itr,
@@ -305,7 +309,7 @@ def validate(args, trainer, task, epoch_itr, subsets):
             tensorboard_logdir=(
                 args.tensorboard_logdir if distributed_utils.is_master(args) else None
             ),
-            default_log_format=('tqdm' if not args.no_progress_bar else 'simple'),
+            default_log_format=("tqdm" if not args.no_progress_bar else "simple"),
         )
 
         # create a new root metrics aggregator so validation metrics
@@ -323,13 +327,12 @@ def validate(args, trainer, task, epoch_itr, subsets):
 
 
 def get_valid_stats(args, trainer, stats):
-    stats['num_updates'] = trainer.get_num_updates()
-    if hasattr(checkpoint_utils.save_checkpoint, 'best'):
-        key = 'best_{0}'.format(args.best_checkpoint_metric)
+    stats["num_updates"] = trainer.get_num_updates()
+    if hasattr(checkpoint_utils.save_checkpoint, "best"):
+        key = "best_{0}".format(args.best_checkpoint_metric)
         best_function = max if args.maximize_best_checkpoint_metric else min
         stats[key] = best_function(
-            checkpoint_utils.save_checkpoint.best,
-            stats[args.best_checkpoint_metric],
+            checkpoint_utils.save_checkpoint.best, stats[args.best_checkpoint_metric]
         )
     return stats
 
@@ -345,14 +348,22 @@ def print_options_meaning_changes(args):
     """Options that have different meanings than those in the translation task
     are explained here.
     """
-    logger.info('--max-tokens is the maximum number of input frames in a batch')
+    logger.info("--max-tokens is the maximum number of input frames in a batch")
 
 
 def cli_main(modify_parser=None):
     parser = options.get_training_parser()
     args = options.parse_args_and_arch(parser, modify_parser=modify_parser)
     print_options_meaning_changes(args)
+    if args.profile:
+        with torch.cuda.profiler.profile():
+            with torch.autograd.profiler.emit_nvtx():
+                cli_main_helper(args)
+    else:
+        cli_main_helper(args)
 
+
+def cli_main_helper(args):
     if args.distributed_init_method is None:
         distributed_utils.infer_init_method(args)
 
@@ -369,29 +380,26 @@ def cli_main(modify_parser=None):
         else:
             distributed_main(args.device_id, args)
     elif args.distributed_world_size > 1:
-        if not getattr(args, 'tpu', False):
+        if not getattr(args, "tpu", False):
             # fallback for single node with multiple GPUs
             assert args.distributed_world_size <= torch.cuda.device_count()
             port = random.randint(10000, 20000)
-            args.distributed_init_method = 'tcp://localhost:{port}'.format(port=port)
+            args.distributed_init_method = "tcp://localhost:{port}".format(port=port)
             args.distributed_rank = None  # set based on device id
             torch.multiprocessing.spawn(
-                fn=distributed_main,
-                args=(args, ),
-                nprocs=args.distributed_world_size,
+                fn=distributed_main, args=(args,), nprocs=args.distributed_world_size
             )
         else:
             import torch_xla.distributed.xla_multiprocessing as xmp
-            torch.multiprocessing.set_sharing_strategy('file_system')
+
+            torch.multiprocessing.set_sharing_strategy("file_system")
             xmp.spawn(
-                fn=distributed_main,
-                args=(args, ),
-                nprocs=8,  # use all 8 TPU cores
+                fn=distributed_main, args=(args,), nprocs=8  # use all 8 TPU cores
             )
     else:
         # single GPU training
         main(args)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     cli_main()
