9b0e8da9486a672c89ce28da4ce159a20f6860c4
freewym <freewym@gmail.com>
2020-06-25 19:54:03 -0400

    code adaptation/changes according to the commits on Jun 24-25, 2020; fix validation loss in LSTM models

--- espresso/data/__init__.py
+++ espresso/data/__init__.py
@@ -3,6 +3,7 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
+from .asr_bucket_pad_length_dataset import FeatBucketPadLengthDataset, TextBucketPadLengthDataset
 from .asr_chain_dataset import AsrChainDataset, NumeratorGraphDataset
 from .asr_dataset import AsrDataset
 from .asr_dictionary import AsrDictionary
@@ -15,14 +16,16 @@ from .feat_text_dataset import (
 )
 
 __all__ = [
-    'AliScpCachedDataset',
-    'AsrChainDataset',
-    'AsrDataset',
-    'AsrDictionary',
-    'AsrTextDataset',
-    'AsrXentDataset',
-    'FeatScpCachedDataset',
-    'FeatScpDataset',
-    'FeatScpInMemoryDataset',
-    'NumeratorGraphDataset',
+    "AliScpCachedDataset",
+    "AsrChainDataset",
+    "AsrDataset",
+    "AsrDictionary",
+    "AsrTextDataset",
+    "AsrXentDataset",
+    "FeatBucketPadLengthDataset",
+    "FeatScpCachedDataset",
+    "FeatScpDataset",
+    "FeatScpInMemoryDataset",
+    "NumeratorGraphDataset",
+    "TextBucketPadLengthDataset",
 ]
