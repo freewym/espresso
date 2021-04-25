6fdea835bb0023edc2f4682d78dd16236c886fc6
Yiming Wang <freewym@gmail.com>
2021-04-25 00:13:32 -0400

    hydra migration for librispeech torchaudio recipes (#65)

--- examples/asr_librispeech/path.sh
+++ examples/asr_librispeech/path.sh
@@ -3,7 +3,7 @@ export KALDI_ROOT=$MAIN_ROOT/espresso/tools/kaldi
 
 # BEGIN from kaldi path.sh
 [ -f $KALDI_ROOT/tools/env.sh ] && . $KALDI_ROOT/tools/env.sh
-export PATH=$PWD/utils/:$KALDI_ROOT/tools/openfst/bin:$KALDI_ROOT/tools/sctk/bin:$PWD:$PATH
+export PATH=$PWD/utils/:$KALDI_ROOT/tools/openfst/bin:$KALDI_ROOT/tools/sctk/bin:$PWD:$MAIN_ROOT:$PATH
 [ ! -f $KALDI_ROOT/tools/config/common_path.sh ] && echo >&2 "The standard file $KALDI_ROOT/tools/config/common_path.sh is not present -> Exit!" && exit 1
 . $KALDI_ROOT/tools/config/common_path.sh
 export LC_ALL=C
