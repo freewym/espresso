bae1c0ada6b293aa144b64d7251bb022730db1d0
freewym <freewym@gmail.com>
2021-10-13 17:41:32 -0400

    code adaptation/changes according to the commits on Oct 13, 2021

--- espresso/models/transformer/speech_transformer_config.py
+++ espresso/models/transformer/speech_transformer_config.py
@@ -8,16 +8,17 @@ import re
 from dataclasses import dataclass, field, fields
 from typing import List, Optional
 
+from omegaconf import II
+
 from fairseq import utils
-from fairseq.dataclass import FairseqDataclass, ChoiceEnum
+from fairseq.dataclass import ChoiceEnum, FairseqDataclass
 from fairseq.models.transformer.transformer_config import (
     DEFAULT_MIN_PARAMS_TO_WRAP,
     EncDecBaseConfig,
     QuantNoiseConfig,
     TransformerConfig,
 )
-from omegaconf import II
-
+from fairseq.utils import safe_getattr, safe_hasattr
 
 DEFAULT_MAX_SOURCE_POSITIONS = 10240
 DEFAULT_MAX_TARGET_POSITIONS = 1024
@@ -222,14 +223,14 @@ class SpeechTransformerConfig(FairseqDataclass):
     def __getattr__(self, name):
         match = re.match(_NAME_PARSER, name)
         if match:
-            sub = getattr(self, match[1])
-            return getattr(sub, match[2])
+            sub = safe_getattr(self, match[1])
+            return safe_getattr(sub, match[2])
         raise AttributeError(f"invalid argument {name}.")
 
     def __setattr__(self, name, value):
         match = re.match(_NAME_PARSER, name)
         if match:
-            sub = getattr(self, match[1])
+            sub = safe_getattr(self, match[1])
             setattr(sub, match[2], value)
         else:
             super().__setattr__(name, value)
@@ -254,7 +255,7 @@ class SpeechTransformerConfig(FairseqDataclass):
                 # concretelly, the transformer_config know what sub-dc it has, so we go through all the dc fields
                 # and if it's one that has a sub-dc, we build that sub-dc with `copy_keys()`
                 if fld.name == "decoder":
-                    if hasattr(args, "decoder"):
+                    if safe_hasattr(args, "decoder"):
                         #  in some cases, the args we receive is already structured (as DictConfigs), so let's just build the correct DC
                         seen.add("decoder")
                         config.decoder = SpeechDecoderConfig(**args.decoder)
@@ -264,7 +265,7 @@ class SpeechTransformerConfig(FairseqDataclass):
                         )
                 elif fld.name == "encoder":
                     # same but for encoder
-                    if hasattr(args, "encoder"):
+                    if safe_hasattr(args, "encoder"):
                         seen.add("encoder")
                         config.encoder = SpeechEncoderConfig(**args.encoder)
                     else:
@@ -273,22 +274,28 @@ class SpeechTransformerConfig(FairseqDataclass):
                         )
                 elif fld.name == "quant_noise":
                     # same but for quant_noise
-                    if hasattr(args, "quant_noise"):
+                    if safe_hasattr(args, "quant_noise"):
                         seen.add("quant_noise")
                         config.quant_noise = QuantNoiseConfig(**args.quant_noise)
                     else:
                         config.quant_noise = cls._copy_keys(
                             args, QuantNoiseConfig, "quant_noise", seen
                         )
-                elif hasattr(args, fld.name):
+                elif safe_hasattr(args, fld.name):
                     # if it's not a structure field, it's just a normal field, copy it over
                     seen.add(fld.name)
-                    setattr(config, fld.name, getattr(args, fld.name))
+                    setattr(config, fld.name, safe_getattr(args, fld.name))
             # we got all the fields defined in the dataclass, but
             # the argparse namespace might have extra args for two reasons:
             #   - we are in a legacy class so all the args are not declared in the dataclass. Ideally once everyone has defined a dataclass for their model, we won't need this
             #   - some places expect args to be there but never define them
-            args_dict = args._asdict() if hasattr(args, '_asdict') else vars(args) if hasattr(args, '__dict__') else {}  # namedtupled doesn't have __dict__ :-/
+            args_dict = (
+                args._asdict()
+                if safe_hasattr(args, "_asdict")
+                else vars(args)
+                if safe_hasattr(args, "__dict__")
+                else {}
+            )  # namedtupled doesn't have __dict__ :-/
             for key, value in args_dict.items():
                 if key not in seen:
                     setattr(config, key, value)
