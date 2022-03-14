16900d93e917ae16d23669a967e5e9e1a4950c73
freewym <freewym@gmail.com>
2022-03-14 01:12:46 -0400

    adaptive specaugment

--- espresso/__init__.py
+++ espresso/__init__.py
@@ -4,6 +4,7 @@
 # LICENSE file in the root directory of this source tree.
 
 import espresso.criterions  # noqa
+import espresso.data.feature_transforms  # noqa
 import espresso.models  # noqa
 import espresso.modules  # noqa
 import espresso.optim  # noqa
